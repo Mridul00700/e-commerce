a5a93f969a9223cd029cb3ee9344216f769d8d1d
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2020-12-07 15:45:45 +0530

    material

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
+import classes from '*.module.css';
 
 
 const Product = () => {
     return (
-        <div>
-            
-        </div>
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image='' title={product.name}/>
+            <CardContent >
+                <div className={classes.cardContent}>
+                    <Typography variant="h5">
+
+                    </Typography>
+
+                </div>
+            </CardContent>
+
+
+
+
+        </Card>
     )
 }
 
