1af3f0b350439e09f1f93b1529b76e92fa660771
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2020-12-21 02:04:11 +0530

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import classes from '*.module.css';
+import logo from '../../assets/download.png';
+ 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit" >
+                        <img src={} alt="Commerce" height="25px" className={classes.image} />
+                        E-Commerce
+                    </Typography>
+                </Toolbar>
+
+            </AppBar>
+        </>
     )
 }
 
