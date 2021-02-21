66a17cb6074bbb68cc3be7248deeaab59ba906b6
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-02-22 01:31:44 +0530

    newComp

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,44 @@
-import React from 'react'
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
-import { ShoppingCart } from '@material-ui/icons';
-import classes from '*.module.css';
-import useStyles from './styles';
-import logo from '../../assets/download.png';
- 
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import classes from "*.module.css";
+import useStyles from "./styles";
+import logo from "../../assets/download.png";
+
 const Navbar = () => {
-    return (
-        <>
-            <AppBar position="fixed" className={classes.appBar} color="inherit">
-                <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit" >
-                        <img src={} alt="Commerce" height="25px" className={classes.image} />
-                        E-Commerce
-                    </Typography>
-                    <div className={classes.grow}></div>
-                    <div className={classes.button}>
-                        <IconButton aria-label="Show cart items" color="inherit">
-                            <Badge badgeContent={2} color="secondary" >
-                                <ShoppingCart />
-                            </Badge>
-                        </IconButton>
-                    </div>
-                </Toolbar>
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src={}
+              alt="Commerce"
+              height="25px"
+              className={classes.image}
+            />
+            E-Commerce
+          </Typography>
+          <div className={classes.grow}></div>
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
-            </AppBar>
-        </>
-    )
-}
-
-export default Navbar
+export default Navbar;
